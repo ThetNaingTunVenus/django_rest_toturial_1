52c611fc88319a1b5416e819d0ec9f7536aa35c6
DELL <kothetnaingtun.ict@gmaiil.com>
2023-08-16 11:04:18 +0630

    test serivaliser

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('article_list/', article_list),
-    # path('myapp/<int:pk>/', views.snippet_detail),
+    path('article_detail/<int:pk>/', article_detail),
 ]
